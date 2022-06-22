43d223d0d3dab2fe87fc0e1f34162a14742f79bd
seanjungmsba <seanjeong1996@gmail.com>
2022-06-22 15:00:56 -0400

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,20 @@
+//Make sure the case that outputs "0 year(s)" doesn't run anymore
+// hint: it has to do with the way currentYear is initialized
+// Add an if statement inside the while loop so the output 
+// can have the right spelling for "year(s)" and "have" for every case.
+
 public class Main {
     public static void main(String[] args) {
         // simulate the passing of time
         int startingYear = 2000;
         int targetYear = 2011;
-        int currentYear = startingYear;
-        while (currentYear < targetYear) {
-            System.out.println((currentYear - startingYear) + " year(s) have passed");
-            // conditional logic based on the current year
+        int currentYear = startingYear + 1;
+        while (currentYear < targetYear) { // conditional logic based on the current year
+            if (currentYear - startingYear == 1)
+                System.out.println((currentYear - startingYear) + " year has passed");
+            else
+                System.out.println((currentYear - startingYear) + " years have passed");
+            
             currentYear++;
         }
     }
